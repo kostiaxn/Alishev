f5f545254b90b3fcc0d3046df9b1528e019e89ea
kosti <kostia.xn@gmail.com>
2023-04-18 12:34:59 +0300

    Reading From File

---
+++ src/ReadingFromFile/ReadingFromFileSecond.java
@@ -0,0 +1,27 @@
+package ReadingFromFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadingFromFileSecond {
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        File file = new File("TestSecond.txt");
+
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        String[] numbersString = line.split(" ");
+        int[] numbers = new int[3];
+        int counter = 0;
+
+        for (String number : numbersString) {
+            numbers[counter++] = Integer.parseInt(number);
+        }
+
+        System.out.println(Arrays.toString(numbers));
+        scanner.close();
+    }
+}
