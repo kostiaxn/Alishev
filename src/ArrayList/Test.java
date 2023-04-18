1dfa2dcbd279adc62c27852dcf94592b6a4b7a0d
kosti <kostia.xn@gmail.com>
2023-04-18 17:43:40 +0300

    ArrayList

---
+++ src/ArrayList/Test.java
@@ -0,0 +1,29 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            list.add(i);
+        }
+        System.out.println(list);
+
+        System.out.println(list.get(0));
+        System.out.println(list.get(9));
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+        for(Integer x : list){
+            System.out.println(x);
+        }
+
+        list.remove(5);
+        System.out.println(list);
+    }
+}
