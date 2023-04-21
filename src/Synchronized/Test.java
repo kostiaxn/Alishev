88b21d8913f8bd2a887db1dd8ef768fa258ffa2d
kosti <kostia.xn@gmail.com>
2023-04-21 12:38:01 +0300

    Synchronized

--- src/Synchronized/Test.java
+++ src/Synchronized/Test.java
@@ -8,22 +8,35 @@ public class Test {
         Test test = new Test();
         test.doWork();
     }
-    public synchronized void increment(){ // synchronized может быть только в методе. Метод синхронизованный
+    public synchronized void increment() throws InterruptedException {// synchronized может быть только в методе. Метод синхронизованный
+        Thread.sleep(1000);
         counter++;
     }
     public void doWork() throws InterruptedException {
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 10000; i++)
-                    increment();
+                for (int i = 0; i < 10000; i++) {
+                    try {
+                        increment();
+                        System.out.println(i);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         });
         Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 10000; i++)
-                    increment();
+                for (int i = 0; i < 10000; i++) {
+                    try {
+                        increment();
+                        System.out.println(i);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
 //                    counter = counter + 1; // counter++ операция не атомарна. Работает не в один такт.
             }
         });
