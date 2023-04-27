6abbd463bd0998cc48af1b1d3263730be12ecd80
kosti <kostia.xn@gmail.com>
2023-04-27 12:51:09 +0300

    PatternAndMatcher

---
+++ src/PatternAndMatcher/Test.java
@@ -0,0 +1,17 @@
+package PatternAndMatcher;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test {
+    public static void main(String[] args) {
+        String text = "Hello, Guys! i send you my email joe@gmail.com so we can\n" +
+                "keep in touch. Thanks, Joe! That's cool. I am sending you\n" +
+                "my address: tim@yandex.ru. Let's stay in touch...";
+        Pattern email = Pattern.compile("(\\w+)@(gmail|yandex)\\.(com|ru)");
+        Matcher matcher = email.matcher(text);
+
+        while (matcher.find())
+            System.out.println(matcher.group());
+    }
+}
