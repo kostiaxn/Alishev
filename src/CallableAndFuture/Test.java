784c2ae5bddc0ff2f8ff1665ddcc2c7955aaddd1
kosti <kostia.xn@gmail.com>
2023-04-27 10:38:39 +0300

    CallableAndFuture с лямда

--- src/CallableAndFuture/Test.java
+++ src/CallableAndFuture/Test.java
@@ -8,20 +8,22 @@ public class Test {
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(1);
-        Future<Integer> future = executorService.submit(new Callable<Integer>() { // когда поток закончит, результат будет в future
-
-            @Override
-            public Integer call() throws Exception {
-                System.out.println("Starting");
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-                System.out.println("Finished");
-                Random random = new Random();
-                return random.nextInt(10);
+        // когда поток закончит, результат будет в future
+        Future<Integer> future = executorService.submit(() -> { // если мы что-то возвращаем, то лямда использует Callable,
+            // если не возвращаем, то Runnable
+            System.out.println("Starting");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e){
+                e.printStackTrace();
             }
+            System.out.println("Finished");
+            Random random = new Random();
+            int randomValue = random.nextInt();
+            if(randomValue < 5)
+                throw new Exception("Something bad happened");
+
+            return random.nextInt(10);
         });
 
         executorService.shutdown();
@@ -29,9 +31,10 @@ public class Test {
             int result = future.get(); // get дожидается окончания выполнения потока
             System.out.println(result);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         } catch (ExecutionException e) {
-            throw new RuntimeException(e);
+            Throwable ex = e.getCause();
+            System.out.println(ex.getMessage());
         }
     }
 }
